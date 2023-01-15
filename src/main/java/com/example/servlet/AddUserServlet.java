96ec2b9a9da2dbd2bc513f815cb5eb3aefb0c288
EDyugaeva <ed2408@yandex.ru>
2023-01-15 18:01:42 +0500

    first

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,44 @@
 package com.example.servlet;
 
-public class AddUserServlet {
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/add")
+
+public class AddUserServlet extends HttpServlet {
     //write your code here!
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        HttpSession session = req.getSession();
+
+        req.setAttribute("name", getRequestParameter(req, "name"));
+        req.setAttribute("lastName", getRequestParameter(req, "lastName"));
+
+        Warehouse warehouse = Warehouse.getInstance();
+
+        User newUser = new User((String) req.getAttribute("name"), (String) req.getAttribute("lastName"));
+        warehouse.addUser(newUser);
+
+
+
+
+    }
+
+    protected String getRequestParameter(
+            HttpServletRequest request,
+            String name) {
+        String param = request.getParameter(name);
+        return !param.isEmpty() ? param : getInitParameter(name);
+    }
 }
--- src/main/java/com/example/servlet/GetUsersServlet.java
+++ src/main/java/com/example/servlet/GetUsersServlet.java
@@ -1,5 +1,41 @@
 package com.example.servlet;
 
-public class GetUsersServlet {
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Set;
+
+@WebServlet("/users")
+
+public class GetUsersServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Warehouse warehouse = Warehouse.getInstance();
+        resp.setContentType("text/html");
+        PrintWriter printWriter = resp.getWriter();
+        Set<User> set = warehouse.getUsers();
+        if (!set.isEmpty()) {
+            for (User user :
+                    set) {
+                printWriter.write(user.getFirstName() + " ");
+                printWriter.write(user.getLastName());
+                printWriter.println();
+            }
+        }
+
+        req.getSession();
+        printWriter.close();
+
+    }
+
     //write your code here!
 }
